e8427de4c2577c946d021b896ee997b9797efcb6
Adam <bgisgone@gmail.com>
2024-07-30 12:04:17 +0800

    Initial Commit

---
+++ FunnyStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunnyStory
+//
+//  Created by Adam Chen on 2024/7/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
